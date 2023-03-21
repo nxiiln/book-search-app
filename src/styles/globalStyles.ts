c9c3fd116e87902eff6badeb28292f77a5210e74
nxiiln <barbashin.nv@gmail.com>
2023-03-22 02:04:18 +0300

    add global styles

--- src/styles/globalStyles.ts
+++ src/styles/globalStyles.ts
@@ -11,10 +11,28 @@ const GlobalStyle = createGlobalStyle`
     box-sizing: border-box;
   }
 
+  *::selection {
+    background: var(--color-accent-second);
+  }
+
   body {
     margin: 0;
     font-family: var(--font-main);
     background: var(--color-second);
+
+    &::-webkit-scrollbar {
+      width: 7px;
+      height: 7px;
+    }
+
+    &::-webkit-scrollbar-thumb {
+      background: var(--color-fade);
+    }
+
+    img {
+      filter: brightness(0.9);
+      user-select: none;
+    }
   }
 `
 
