f332787d96f0033d357e3e03d2743bb772c1feef
nxiiln <barbashin.nv@gmail.com>
2023-03-22 02:04:47 +0300

    add custom style

--- src/components/Select.ts
+++ src/components/Select.ts
@@ -4,7 +4,27 @@ import styled from 'styled-components';
 const Select = styled.select`
   width: 100px;
   height: 30px;
+  font-family: var(--font-main);
+  font-size: 16px;
+  color: var(--color-main);
+  background: var(--color-fade);
+  border: none;
   cursor: pointer;
+  outline: none;
+  transition: all ease-out 0.2s;
+
+  &:hover, &:focus-visible {
+    background: var(--color-accent);
+    transform: scale(1.07);
+  }
+
+  & > option {
+    font-family: var(--font-main);
+    font-size: 16px;
+    color: var(--color-main);
+    background: var(--color-fade);
+    cursor: pointer;
+  }
 `
 
 
