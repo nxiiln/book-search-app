c5fac53fbb698763d4c8eb1601a27dace1c5f0ba
nxiiln <barbashin.nv@gmail.com>
2023-03-21 09:50:22 +0300

    create search feature

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,14 @@
+import { useState } from 'react';
+import { useAppDispatch } from '../redux-hooks';
 import styled from 'styled-components';
 import Title from './Title';
 import Input from './Input';
 import Button from './Button';
 import Text from './Text';
 import Select from './Select';
+import { clearState, fetchBooks } from '../slices/books';
+import { setBaseQuery } from '../slices/query';
+import { CATEGORIES, SORTING, URL_BASE, MAX_RESULTS } from '../constants';
 
 
 const HeaderWrapperOuter = styled.header`
@@ -40,39 +45,72 @@ const SelectGroup = styled.div`
 `
 
 
-const categories: string[] = ['all', 'art', 'biography', 'computers', 'history', 'medical', 'poetry']
-const sorting: string[] = ['relevance', 'newest']
-
-const handleSubmit = (e: React.FormEvent<HTMLFormElement>): void => e.preventDefault()
-
-const fillSelect = (options: string[]): JSX.Element[] => (
-  options.map((option: string, id: number): JSX.Element => (
-    <option key={id}>{option}</option>
-  ))
-)
 
 
-const Header = (): JSX.Element => (
-  <HeaderWrapperOuter>
-    <HeaderWrapperInner>
-      <Title>Search Book 📚</Title>
-      <Form onSubmit={handleSubmit}>
-        <Input type='search' />
-        <Button>Search 🔍</Button>
+const Header = (): JSX.Element => {
+  const [query, setQuery] = useState<string>('')
+  const [category, setCategory] = useState<string>('all')
+  const [sort, setSort] = useState<string>('relevance')
+  const dispatch = useAppDispatch()
 
-        <SelectGroup>
-          <Text>Categories</Text>
-          <Select>{fillSelect(categories)}</Select>
-        </SelectGroup>
+  const subject: string = category === 'all' ? '' : `+subject:${category}`
+  const orderBy: string = `&orderBy=${sort}`
+  const baseQuery: string = URL_BASE + query + subject + orderBy
+  const urlQuery: string = `${baseQuery}&startIndex=0&maxResults=${MAX_RESULTS}`
 
-        <SelectGroup>
-          <Text>Sorting by</Text>
-          <Select>{fillSelect(sorting)}</Select>
-        </SelectGroup>
-      </Form>
-    </HeaderWrapperInner>
-  </HeaderWrapperOuter>
-)
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>): void => {
+    e.preventDefault()
+    dispatch(clearState())
+    dispatch(setBaseQuery(baseQuery))
+    query && dispatch(fetchBooks(urlQuery))
+  }
+
+  const handleChangeInput = (e: React.ChangeEvent<HTMLInputElement>): void => {
+    setQuery(e.target.value)
+  }
+
+  const handleChangeSelect = (setValue: (value: string) => void) => (
+    (e: React.ChangeEvent<HTMLSelectElement>): void => {
+      setValue(e.target.value)
+    }
+  )
+
+  const fillSelect = (options: string[]): JSX.Element[] => (
+    options.map((option: string, id: number): JSX.Element => (
+      <option key={id} value={option}>{option}</option>
+    ))
+  )
+
+  return (
+    <HeaderWrapperOuter>
+      <HeaderWrapperInner>
+        <Title>Search Book 📚</Title>
+        <Form onSubmit={handleSubmit}>
+          <Input
+            type='search'
+            value={query}
+            onChange={handleChangeInput}
+          />
+          <Button>Search 🔍</Button>
+
+          <SelectGroup>
+            <Text>Categories</Text>
+            <Select value={category} onChange={handleChangeSelect(setCategory)}>
+              {fillSelect(CATEGORIES)}
+            </Select>
+          </SelectGroup>
+
+          <SelectGroup>
+            <Text>Sorting by</Text>
+            <Select value={sort} onChange={handleChangeSelect(setSort)}>
+              {fillSelect(SORTING)}
+            </Select>
+          </SelectGroup>
+        </Form>
+      </HeaderWrapperInner>
+    </HeaderWrapperOuter>
+  )
+}
 
 
 export default Header
