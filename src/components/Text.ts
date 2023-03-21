79ca62d431ceab2897acd369017906e6b0b7b237
nxiiln <barbashin.nv@gmail.com>
2023-03-21 09:50:01 +0300

    add text-align to Text

--- src/components/Text.ts
+++ src/components/Text.ts
@@ -1,9 +1,14 @@
 import styled from 'styled-components';
 
+type TTextProps = {
+  fontSize?: string,
+  textAlign?: string,
+}
 
-const Text = styled.span<{fontSize?: string}>`
+const Text = styled.span<TTextProps>`
   font-family: var(--font-main);
   font-size: ${props => props.fontSize || '16px'};
+  text-align: ${props => props.textAlign || 'start'};
   color: var(--color-main);
 `
 
