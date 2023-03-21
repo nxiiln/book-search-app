2e9e8d966ba77bc66f4fa2ded0f988d8950bc6e3
nxiiln <barbashin.nv@gmail.com>
2023-03-22 02:05:19 +0300

    add transition

--- src/components/Button.ts
+++ src/components/Button.ts
@@ -4,21 +4,25 @@ import styled from 'styled-components';
 type TButtonProps = {
   width?: string,
   height?: string,
+  margin?: string,
 }
 
 const Button = styled.button<TButtonProps>`
   width: ${props => props.width || '100px'};
   height: ${props => props.height || '30px'};
+  margin: ${props => props.margin};
   font-family: var(--font-main);
   font-size: 16px;
   color: var(--color-main);
   background: var(--color-fade);
-  border: 1px solid var(--color-main);
+  border: none;
   outline: none;
   cursor: pointer;
+  transition: all ease-out 0.2s;
 
   &:hover, &:focus-visible {
     background: var(--color-accent);
+    transform: scale(1.07);
   }
 
   &:active {
--- src/components/Input.ts
+++ src/components/Input.ts
@@ -7,17 +7,19 @@ type TInputProps = {
 }
 
 const Input = styled.input<TInputProps>`
-  width: ${props => props.width || '70%'};
+  width: ${props => props.width || '60%'};
   height: ${props => props.height || '30px'};
   font-family: var(--font-main);
   font-size: 16px;
   color: var(--color-main);
   background: var(--color-fade);
-  border: 1px solid var(--color-main);
+  border: none;
   outline: none;
+  box-shadow: 0;
+  transition: all ease-out 0.1s;
 
   &:focus-visible {
-    border-color: var(--color-accent);
+    box-shadow: 0 0 0 1px var(--color-accent);
   }
 `
 
