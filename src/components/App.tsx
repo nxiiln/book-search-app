55a4f3394db50dbf8adf9fb2c561c948a0c99448
nxiiln <barbashin.nv@gmail.com>
2023-03-21 20:09:54 +0300

    create routing

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,11 +1,16 @@
+import { Routes, Route } from 'react-router-dom';
 import Header from './Header';
 import Main from './Main';
+import BookDetail from './BookDetail';
 
 
 const App = (): JSX.Element => (
   <>
     <Header />
-    <Main />
+    <Routes>
+      <Route path='/' element={<Main />} />
+      <Route path='/:id' element={<BookDetail />} />
+    </Routes>
   </>
 )
 
